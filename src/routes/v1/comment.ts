1090125e63a40a217ef0a325d1abc2d83d880ad1
Theekshana Nuwan <theekshanabro40@gmail.com>
2025-10-31 00:14:29 +0530

    Add: Comment in a blog

---
+++ src/routes/v1/comment.ts
@@ -0,0 +1,36 @@
+/**
+ * @copyright 2025 nuwandev
+ * @license Apache-2.0
+ */
+
+/**
+ * Node modules
+ */
+import { Router } from 'express';
+import { body, param } from 'express-validator';
+
+/**
+ * Middlewares
+ */
+import authenticate from '@/middlewares/authenticate';
+import authorize from '@/middlewares/authorize';
+import validationError from '@/middlewares/validationError';
+
+/**
+ * Controllers
+ */
+import commentBlog from '@/controllers/v1/comment/comment_blog';
+
+const router = Router();
+
+router.post(
+  '/blog/:blogId',
+  authenticate,
+  authorize(['admin', 'user']),
+  param('blogId').isMongoId().withMessage('Invalid blog ID'),
+  body('content').trim().notEmpty().withMessage('Content is required'),
+  validationError,
+  commentBlog,
+);
+
+export default router;
--- src/routes/v1/index.ts
+++ src/routes/v1/index.ts
@@ -16,6 +16,7 @@ import authRoutes from '@/routes/v1/auth';
 import userRoutes from '@/routes/v1/user';
 import blogRoutes from '@/routes/v1/blog';
 import likeRoutes from '@/routes/v1/like';
+import commentRoutes from '@/routes/v1/comment';
 
 /**
  * Root route
@@ -34,5 +35,6 @@ router.use('/auth', authRoutes);
 router.use('/users', userRoutes);
 router.use('/blogs', blogRoutes);
 router.use('/likes', likeRoutes);
+router.use('/comments', commentRoutes);
 
 export default router;
